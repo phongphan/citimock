492233e94c295a785f343da835f30202e51d8945
phongphan <phongphan.p@gmail.com>
2023-04-09 16:40:47 +0700

    mtls impl with openssl

---
+++ src/handlers/authentication.rs
@@ -0,0 +1,49 @@
+use axum::{
+    async_trait,
+    body::{BoxBody, Bytes},
+    extract::FromRequest,
+    http::Request,
+    response::{IntoResponse, Response},
+    Json};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize)]
+pub struct AuthenticationResponse {
+    email: String,
+    password: String,
+}
+
+#[derive(Deserialize)]
+pub struct AuthenticationRequest {
+    email: String,
+    password: String,
+}
+
+pub async fn authentication_v2(XmlEncBody(body): XmlEncBody) -> Json<AuthenticationResponse> {
+    println!("{:?}", body);
+    Json(AuthenticationResponse{
+        email: "hello".to_string(),
+        password: "world".to_string()
+    })
+}
+
+
+pub struct XmlEncBody(Bytes);
+
+#[async_trait]
+impl<S> FromRequest<S, BoxBody> for XmlEncBody
+where
+    S: Send + Sync,
+{
+    type Rejection = Response;
+
+    async fn from_request(req: Request<BoxBody>, state: &S) -> Result<Self, Self::Rejection> {
+        let body = Bytes::from_request(req, state)
+            .await
+            .map_err(|err| err.into_response())?;
+
+        //do_thing_with_request_body(body.clone());
+
+        Ok(Self(body))
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,141 @@
+use openssl::ssl::{Ssl, SslAcceptor, SslFiletype, SslMethod, SslVerifyMode};
+use openssl::x509::{X509, store::{X509Store, X509StoreBuilder}};
+use tokio_openssl::SslStream;
+use axum::{
+    body::{self, BoxBody, Full},
+    extract::ConnectInfo,
+    http::{header::CONTENT_TYPE, method::Method, Request, StatusCode},
+    middleware::{self, Next},
+    response::{Html, IntoResponse, Response},
+    routing::{get, post},
+    Router};
+use futures_util::future::poll_fn;
+use hyper::server::{
+    accept::Accept,
+    conn::{AddrIncoming, Http},
+};
+use std::{net::SocketAddr, path::PathBuf, pin::Pin, sync::Arc};
+use tokio::net::TcpListener;
+use tower::{MakeService, ServiceBuilder};
+use tower_http::ServiceBuilderExt;
+
+use citimock::handlers::authentication::authentication_v2;
+use citimock::certificates::cert_manager::{KeyContent, KeyStore, SimpleKeyStore};
+
+#[tokio::main]
+async fn main() {
+    let mut ks: SimpleKeyStore = KeyStore::new("default");
+    ks.store(KeyContent::new("", "", "", "", 0, 0));
+    ks.get_by_client("1", "encryption_cert");
+
+
+    // openssl
+    let mut tls_builder = SslAcceptor::mozilla_modern_v5(SslMethod::tls()).unwrap();
+    tls_builder.set_certificate_file("./certs/certificate.crt", SslFiletype::PEM).unwrap();
+    tls_builder.set_private_key_file("./certs/privateKey.key", SslFiletype::PEM).unwrap();
+    tls_builder.check_private_key().unwrap();
+
+    // client verifier
+    // set options to make sure to validate the peer aka mtls
+
+    let my_cert_bytes = std::fs::read("./certs/certificate.crt").unwrap();
+    let my_cert = X509::from_pem(&my_cert_bytes).unwrap();
+    let mut builder = X509StoreBuilder::new().unwrap();
+    let _ = builder.add_cert(my_cert);
+    let store: X509Store = builder.build();
+
+    let mut verify_mode = SslVerifyMode::empty();
+    verify_mode.set(SslVerifyMode::PEER, true);
+    verify_mode.set(SslVerifyMode::FAIL_IF_NO_PEER_CERT, true);
+    tls_builder.set_verify_cert_store(store).unwrap();
+    tls_builder.set_verify(verify_mode);
+    // openssl
+
+    let acceptor = tls_builder.build();
+    let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    let mut listener = AddrIncoming::from_listener(listener).unwrap();
+
+    let protocol = Arc::new(Http::new());
+    let mut app = Router::new()
+        .route("/", get(handler))
+        /*.route("/v2/auth", post(authentication_v2))
+        .layer(
+            ServiceBuilder::new()
+                .map_request_body(body::boxed::<BoxBody>)
+                .layer(middleware::from_fn(validate_content_type)),
+        )*/
+        .into_make_service_with_connect_info::<SocketAddr>();
+
+    loop {
+        let stream = poll_fn(|cx| Pin::new(&mut listener).poll_accept(cx))
+            .await
+            .unwrap()
+            .unwrap();
+
+        let acceptor = acceptor.clone();
+        let protocol = protocol.clone();
+        let svc = app.make_service(&stream);
+
+        tokio::spawn(async move {
+            let ssl = Ssl::new(acceptor.context()).unwrap();
+            let mut tls_stream = SslStream::new(ssl, stream).unwrap();
+
+            SslStream::accept(Pin::new(&mut tls_stream)).await.unwrap();
+
+            let _ = protocol
+                .serve_connection(tls_stream, svc.await.unwrap())
+                .await;
+        });
+    }
+
+    /*let app = Router::new()
+        .route("/", get(handler))
+        .route("/v2/auth", post(authentication_v2))
+        .layer(
+            ServiceBuilder::new()
+                .map_request_body(body::boxed)
+                .layer(middleware::from_fn(validate_content_type)),
+        );
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();*/
+}
+
+async fn handler(ConnectInfo(addr): ConnectInfo<SocketAddr>) -> Html<String> {
+    Html(format!("<h1>Hello, world! {}", addr.to_string()))
+}
+
+async fn validate_content_type(
+    request: Request<BoxBody>,
+    next: Next<BoxBody>,
+) -> Result<impl IntoResponse, Response> {
+    let methods_with_body = [Method::PATCH, Method::POST, Method::PUT];
+    println!("{:?}", request);
+    if methods_with_body.contains(request.method()) {
+        let content_type_header = request.headers().get(CONTENT_TYPE);
+        let content_type = content_type_header.and_then(|value| value.to_str().ok());
+
+        if let Some(content_type) = content_type {
+            if !(content_type.starts_with("application/xml") || content_type.starts_with("text/xml")) {
+                return Err(StatusCode::UNSUPPORTED_MEDIA_TYPE.into_response());
+            }
+        } else {
+            return Err(StatusCode::UNSUPPORTED_MEDIA_TYPE.into_response());
+        }
+    }
+    
+    let (parts, body) = request.into_parts();
+    // this wont work if the body is an long running stream
+    let bytes = hyper::body::to_bytes(body)
+        .await
+        .map_err(|err| (StatusCode::INTERNAL_SERVER_ERROR, err.to_string()).into_response())?;
+
+    //do_thing_with_request_body(bytes.clone());
+
+    let request = Ok(Request::from_parts(parts, body::boxed(Full::from(bytes))))?;
+    Ok(next.run(request).await)
+}
