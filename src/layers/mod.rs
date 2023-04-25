0563e177ee6cc724f63d28a90118928e224990ca
phongphan <phongphan@acm.org>
2023-04-25 13:14:59 +0700

    re-export layer structs

--- src/layers/mod.rs
+++ src/layers/mod.rs
@@ -4,3 +4,9 @@ pub mod document_decryption;
 pub mod document_encryption;
 pub mod document_signature_verifier;
 pub mod document_signing;
+
+pub use self::{
+    authentication::AuthenticationLayer, authentication_check::authentication_check_layer,
+    document_decryption::DecryptionLayer, document_encryption::EncryptionLayer,
+    document_signature_verifier::VerifierLayer, document_signing::SigningLayer,
+};
